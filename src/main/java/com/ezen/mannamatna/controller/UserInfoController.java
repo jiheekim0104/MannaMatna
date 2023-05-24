b6969ae17e6e1344e2df54809a2038b8681ce588
jiheekim0104 <bearitlove@naver.com>
2023-05-24 17:45:58 +0900

    지희 db 연결 확인 완료~

--- src/main/java/com/ezen/mannamatna/controller/UserInfoController.java
+++ src/main/java/com/ezen/mannamatna/controller/UserInfoController.java
@@ -1,14 +1,28 @@
 package com.ezen.mannamatna.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ezen.mannamatna.mapper.UserInfoMapper;
+import com.ezen.mannamatna.service.UserInfoService;
+import com.ezen.mannamatna.vo.UserInfoVO;
 
 @Controller
 public class UserInfoController {
+	
+	@Autowired
+	UserInfoService uiService;
+	
 	@GetMapping("/") 
-	public String home(Model m) {
+	public String home(@ModelAttribute UserInfoVO userInfoVO, Model m) {
+		List<UserInfoVO> userList = uiService.selectUserInfo(userInfoVO);
+		m.addAttribute("userList", userList);
 		return "index"; 
 	}
 	
--- src/main/java/com/ezen/mannamatna/service/UserInfoService.java
+++ src/main/java/com/ezen/mannamatna/service/UserInfoService.java
@@ -1,5 +1,19 @@
 package com.ezen.mannamatna.service;
 
-public class UserInfoService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ezen.mannamatna.mapper.UserInfoMapper;
+import com.ezen.mannamatna.vo.UserInfoVO;
+
+@Service
+public class UserInfoService {
+	@Autowired
+	private UserInfoMapper uiMapper;
+	
+	public List<UserInfoVO> selectUserInfo(UserInfoVO user){
+		return uiMapper.selectUserInfo(user);
+	}
 }
