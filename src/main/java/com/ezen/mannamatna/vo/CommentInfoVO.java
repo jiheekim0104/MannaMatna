c693191c1854b42765d394e82fa2eedfdc5fc44a
i7E-28 <i7E-28@i7E-27>
2023-05-24 17:42:59 +0900

    230524 오성철 commentInfoVO만 만들고 커밋

---
+++ src/main/java/com/ezen/mannamatna/vo/CommentInfoVO.java
@@ -0,0 +1,14 @@
+package com.ezen.mannamatna.vo;
+
+import lombok.Data;
+
+@Data
+public class CommentInfoVO {
+
+	int ciNum;
+	String ciComment;
+	String ciCredat;
+	String ciCretim;
+	int uiNum;
+	int biNum;
+}
