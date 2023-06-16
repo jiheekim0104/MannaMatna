3c520986ce02fe03b802bbe5ddf5386833927f12
chlwo796 <chlwo796@gmail.com>
2023-06-16 17:53:41 +0900

    밥상컨트롤러 참가하기 포스트매핑추가(완성x)

--- src/main/java/com/ezen/mannamatna/controller/BabsangInfoController.java
+++ src/main/java/com/ezen/mannamatna/controller/BabsangInfoController.java
@@ -21,105 +21,121 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 @Log4j2
 public class BabsangInfoController {
-	
+
 	@Autowired
 	private BabsangInfoService babsangInfoService;
-	@Autowired 
+	@Autowired
 	private UserInfoService userInfoService;
-	 
-	
 
-	/* 기존 밥상 리스트 나열할 때 사용
+	/*
+	 * 기존 밥상 리스트 나열할 때 사용
+	 * 
 	 * @GetMapping("/main") public String goMain(BabsangInfoVO babsang, Model m){
 	 * List<BabsangInfoVO> babsangList =
 	 * babsangInfoService.getBabsangInfoVOs(babsang);
 	 * m.addAttribute("babsangList",babsangList); return "babsang/babsang-list"; }
 	 */
-	 
+
 	/* 페이징 */
 	@GetMapping("/main")
-	public String pagingBabsangList(@ModelAttribute BabsangInfoVO babsang, Model m){
+	public String pagingBabsangList(@ModelAttribute BabsangInfoVO babsang, Model m) {
 		m.addAttribute("page", babsangInfoService.getPagingBansang(babsang));
 		return "babsang/babsang-list";
 	}
 
-	
 	@GetMapping("/addBabsang")
-	public String goCreateBabsang(){
+	public String goCreateBabsang() {
 		return "babsang/babsang-insert";
 	}
-	
+
 	@PostMapping("/addBabsang")
-	public String insertBabsang(BabsangInfoVO babsang,  UserInfoVO userInfoVO,  Model m, HttpSession session) {
-		UserInfoVO userSession = (UserInfoVO)session.getAttribute("user");
+	public String insertBabsang(BabsangInfoVO babsang, UserInfoVO userInfoVO, Model m, HttpSession session) {
+		UserInfoVO userSession = (UserInfoVO) session.getAttribute("user");
 
 		babsang.setUiNum(userSession.getUiNum()); // 세션의 uiNum >> 받아서 밥상의 uiNum으로 set
 		log.info("userInfo_biNum ===> {}", babsang.getUiBiNum()); // 밥상이 생성되지않아 받아올게 없어서 0이 나옴
 		String msg = "밥상 등록 실패";
 		String url = "/babsang-insert";
-		
-		if(babsangInfoService.addBabsangInfo(babsang)) {// 밥상이 insert 되면
+
+		if (babsangInfoService.addBabsangInfo(babsang)) {// 밥상이 insert 되면
 			int biNum = babsang.getUiBiNum(); // insert 할 때의 autoIncrement 된 밥상 번호를 받아옴
 			log.info("babsang.uiBiNum ===> {}", biNum); // 확인
-			
+
 			userInfoVO.setUiNum(userSession.getUiNum());
 
 			userInfoVO.setBiNum(biNum); // 유저의 biNum >> 밥상 insert 할 때 받아온 biNum으로 set
 			log.info("userInfo.biNum ===> {}", userInfoVO.getBiNum()); // 확인
-			userSession.setBiNum(biNum); // 세션의 biNum >> 밥상 insert 할 때 받아온 biNum으로 set 
+			userSession.setBiNum(biNum); // 세션의 biNum >> 밥상 insert 할 때 받아온 biNum으로 set
 			log.info("userSession.biNum ===> {}", userSession.getBiNum()); // 확인
-			
+
 			msg = "밥상 등록 성공";
 			url = "/main";
 			userInfoService.updateBiNum(userInfoVO); // insert성공 시 유저서비스의 update 실행
 			log.info("==== 밥상 insert 끝 ====");
 		}
 		m.addAttribute("msg", msg);
-		m.addAttribute("url",url);
+		m.addAttribute("url", url);
 		return "common/msg";
 	}
 
 	@GetMapping("/detail/{biNum}") // biNum으로 VO를 가져오는 방식
-    public String detailBabsang(@PathVariable int biNum, Model m){
+	public String detailBabsang(@PathVariable int biNum, Model m) {
 		// 밥상 상세페이지 컨트롤러 구현
 		BabsangInfoVO babsangInfoVO = babsangInfoService.getBabsangInfoVO(biNum);
-        m.addAttribute("detail", babsangInfoVO);
-        // 모델에 detail로 해당 상세 밥상객체를 넣어준 후
-        m.addAttribute("babsangMaker",userInfoService.getUserInfoFromBabsang(babsangInfoVO.getUiNum()));
-        // 해당 밥상객체의 uiNum으로 밥상작성자 유저를 모델에 넣어준다.
-        m.addAttribute("babsangUserList", userInfoService.getUserInfosByBiNum(biNum));
-        // 참가하기를 누르면 UserInfoVO에 biNum이 업데이트 되며, 해당 biNum과 같은 유저리스트(참가자)를 가져온 후
-        // 모델에 넣어준다.
-        return "babsang/babsang-detail"; // 요청 jsp
-    }
-	
+		m.addAttribute("detail", babsangInfoVO);
+		// 모델에 detail로 해당 상세 밥상객체를 넣어준 후
+		m.addAttribute("babsangMaker", userInfoService.getUserInfoFromBabsang(babsangInfoVO.getUiNum()));
+		// 해당 밥상객체의 uiNum으로 밥상작성자 유저를 모델에 넣어준다.
+		m.addAttribute("babsangUserList", userInfoService.getUserInfosByBiNum(biNum));
+		// 참가하기를 누르면 UserInfoVO에 biNum이 업데이트 되며, 해당 biNum과 같은 유저리스트(참가자)를 가져온 후
+		// 모델에 넣어준다.
+		return "babsang/babsang-detail"; // 요청 jsp
+	}
+
 	@GetMapping("/deleteBabsang")
 	public String deleteBabsang(Model m, @RequestParam("biNum") int biNum, UserInfoVO userInfoVO, HttpSession session) {
-		UserInfoVO userSession = (UserInfoVO)session.getAttribute("user");
+		UserInfoVO userSession = (UserInfoVO) session.getAttribute("user");
 		String msg = "밥상 삭제 실패";
 		String url = "/main";
-		if(babsangInfoService.deleteBabsangInfo(biNum)) {
+		if (babsangInfoService.deleteBabsangInfo(biNum)) {
 			userInfoVO.setUiNum(userSession.getUiNum());
 
-			userSession.setBiNum(0);//세션의 biNum >> 0으로 set
-			log.info("userSession.biNum >>> {}",userSession.getBiNum());
-			
-			userInfoVO.setBiNum(0);//유저의 biNum >> 0으로 set
-			log.info("user.biNum >>> {}",userInfoVO.getBiNum());
-			
+			userSession.setBiNum(0);// 세션의 biNum >> 0으로 set
+			log.info("userSession.biNum >>> {}", userSession.getBiNum());
+
+			userInfoVO.setBiNum(0);// 유저의 biNum >> 0으로 set
+			log.info("user.biNum >>> {}", userInfoVO.getBiNum());
+
 			msg = "밥상 삭제 성공";
 			url = "/main";
 			userInfoService.updateBiNum(userInfoVO); // delete 성공 시 유저서비스의 update 실행
 		}
-		m.addAttribute("msg",msg);
-		m.addAttribute("url",url);
+		m.addAttribute("msg", msg);
+		m.addAttribute("url", url);
 		return "common/msg";
 	}
-	
+
 	// 비로그인 시 밥상 보려고 할 때
 	@GetMapping("/cannotSeeBabsang")
 	public String cannotSeeBabsang() {
 		return "common/common";
 	}
-	
+
+	@PostMapping("/babsangJoin")
+	public String joinBabsang(Model m, @RequestParam("biNum") int biNum, HttpSession session) {
+		// 참가하기 버튼을 누르면 해당 메소드로 포스트매핑된 후 알림메세지와함께 redirect되도록해보겠다!!
+		// biNum은 모델에 담겨있을거같은데???
+		// 주말에 실험해본다!!
+		BabsangInfoVO babsangInfoVO = (BabsangInfoVO) m.getAttribute("detail");
+		log.info("상세페이지에 담겨있는 모델의 밥상객체 확인해보자!!{}", babsangInfoVO);
+		log.info("참가하기누르고나서 biNum확인하기!!{}", biNum);
+		UserInfoVO userInfoVO = (UserInfoVO) session.getAttribute("user");
+		// 세션에서 uiVO 객체를 제공받은 후 해당 객체로 uiService의 biNum업데이트 실행
+		if (userInfoService.updateBiNum(userInfoVO)) {
+			// 서비스가 정상적으로 실행됐을 경우
+		} else {
+			// 정상실행 아닐경우
+		}
+		return "common/msg";
+	}
 }
